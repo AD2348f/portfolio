c94385232eff3f1d3d5c418bf4d3dd35d496b92b
Alexander Ditz <77445887+AD2348f@users.noreply.github.com>
2021-09-01 14:10:23 +0200

    added cv

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,7 @@ import './Content.css';
 import '../App.css';
 import "../assets.css";
 import portrait from "../pictures/Portrait1.png";
+import cv from "../pictures/CV Alexander Ditz 20210901.pdf";
 import { ImGithub, ImLinkedin, ImMail4, ImFileEmpty, ImHtmlFive, ImCss3 } from "react-icons/im";
 
 
@@ -22,7 +23,7 @@ return (
         <a className="App__Header-Button" href="https://github.com/AD2348f" target="_blank"><ImGithub size={'3rem'}/>GitHub</a>
         <a className="App__Header-Button" href="https://www.linkedin.com/in/alexander-ditz/" target="_blank"><ImLinkedin size={'3rem'} color={'#0d47a1'}/>LinkedIn</a>
         <a className="App__Header-Button" href="mailto:ditz.alexander@gmx.de"><ImMail4 size={'3rem'} color={'#b80000'}/>E-Mail</a>
-        <a className="App__Header-Button" href="../pictures/CV Alexander Ditz 20210901.pdf" target="_blank"><ImFileEmpty size={'3rem'} color={'#008b02'}/>CV</a>        
+        <a className="App__Header-Button" href={cv} target="_blank"><ImFileEmpty size={'3rem'} color={'#008b02'}/>CV</a>        
         </div>
         {/* <div className="App__Header-Moreinfo-Buttons-Wrapper">
         <button className="App__Header-Moreinfo-Button" href="#techStack">Tech Stack</button>
